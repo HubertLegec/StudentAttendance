f2c6763c0704f743380b2a283add1b9b271afba2
HubertLegec <hubert.legec@gmail.com>
2017-04-08 03:50:52 +0200

    floating button

--- app/src/main/kotlin/com/legec/studentattendance/semester/SemesterActivity.kt
+++ app/src/main/kotlin/com/legec/studentattendance/semester/SemesterActivity.kt
@@ -1,20 +1,31 @@
 package com.legec.studentattendance.semester
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
+import android.os.Environment
+import android.provider.MediaStore
 import android.support.design.widget.Snackbar
 import android.support.design.widget.TabLayout
 import android.support.v4.app.FragmentPagerAdapter
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import butterknife.BindView
 import butterknife.ButterKnife
+import butterknife.OnClick
 import com.legec.studentattendance.R
+import java.io.File
+import java.io.IOException
 
 class SemesterActivity : AppCompatActivity() {
+    private val TAG = "SemesterActivity"
+    private val REQUEST_TAKE_PHOTO = 0
     @BindView(R.id.toolbar)
     lateinit var toolbar: Toolbar
     /**
@@ -32,6 +43,8 @@ class SemesterActivity : AppCompatActivity() {
      */
     private var mViewPager: ViewPager? = null
 
+    private var takenPhotoUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_semester)
@@ -47,13 +60,29 @@ class SemesterActivity : AppCompatActivity() {
 
         val tabLayout = findViewById(R.id.tabs) as TabLayout
         tabLayout.setupWithViewPager(mViewPager)
+    }
 
-        val fab = findViewById(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+    @OnClick(R.id.camera_button)
+    fun onCameraButtonClick(view: View) {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (intent.resolveActivity(packageManager) != null) {
+            val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+            try {
+                val file = File.createTempFile("IMG_", ".jpg", storageDir)
+                takenPhotoUri = Uri.fromFile(file)
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, takenPhotoUri)
+                startActivityForResult(intent, REQUEST_TAKE_PHOTO)
+            } catch (e: IOException) {
+                Log.e(TAG, e.message)
+                Snackbar.make(view, "Problem with creating image file", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show()
+            }
         }
+    }
 
+    @OnClick(R.id.gallery_button)
+    fun onGalleryButtonClick() {
+        //TODO
     }
 
 
@@ -75,5 +104,16 @@ class SemesterActivity : AppCompatActivity() {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == Activity.RESULT_OK) {
+                val imageUri: Uri
+                if (data == null || data.data == null) {
+                    imageUri = takenPhotoUri!!
+                } else {
+                    imageUri = data.data
+                }
+        }
     }
 }
